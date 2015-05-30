335ca9fe316526d56f83370d942a7b929aea4de7
Safin Roman <r.n.safin@gmail.com>
2015-05-30 10:25:33 +0300

    added some examples

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+// bootstrap.php
+//php vendor\doctrine\orm\bin\doctrine.php orm:schema-tool:update --force
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+// or if you prefer yaml or XML
+//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
+//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+
+// database configuration parameters
+$conn = array(
+    'driver'   => 'pdo_mysql',
+    'host'     => '127.0.0.1',
+    'dbname'   => 'doctrine2-tutorial',
+    'user'     => 'root',
+    'password' => ''
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
